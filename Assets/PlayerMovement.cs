812e8b995f925c11722a02e5e10e57b0d4b40dd5
bokke1010 <bokke1010@gmail.com>
2023-08-23 11:36:26 +0200

    Improve player movement Add mouse aiming

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D playerbody;
     public float speed = 4.0f;
+    public float acceleration = 1f;
     public float jumpstrenght = 4.5f;
     public float launchspeed = 12f;
     public Transform egg;
@@ -21,37 +22,42 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         if (Input.GetButtonDown("Jump")) {
-            Debug.Log("Jump");
             playerbody.AddForce(Vector2.up * jumpstrenght);
         }
-        if (Input.GetKeyDown(KeyCode.E)) {
+        if (Input.GetMouseButtonDown(0)) {
             if (lastegg) {
                 GameObject.Destroy(lastegg);
             }
-            Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-            if (direction.sqrMagnitude < 0.1) {
-                direction = Vector3.down;
-            }
+            
+            Vector2 aimdir = (Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+            aimdir.Normalize();
 
-            direction.Normalize();
-
-
-            Transform newegg = Instantiate(egg, gameObject.transform.position + direction, gameObject.transform.rotation);
+            Transform newegg = Instantiate(egg, gameObject.transform.position + (Vector3)aimdir, gameObject.transform.rotation);
             Rigidbody2D eggbody = newegg.GetComponent<Rigidbody2D>();
             eggbody.velocity = playerbody.velocity;
-            eggbody.AddForce(launchspeed * (Vector2)direction);
-            playerbody.AddForce(-launchspeed * (Vector2)direction);
+            eggbody.AddForce(launchspeed * aimdir);
+            playerbody.AddForce(-launchspeed * aimdir);
             lastegg = newegg.gameObject;
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalVelocity = horizontalInput * speed;
+        playerbody.velocity = new Vector2(
+            Mathf.Clamp(
+                horizontalVelocity,
+                playerbody.velocity.x - Time.deltaTime * acceleration,
+                playerbody.velocity.x + Time.deltaTime * acceleration
+            ), playerbody.velocity.y
+        );
+
+        if (Mathf.Abs(horizontalInput) < 0.2) {
+            playerbody.velocity = Vector2.ClampMagnitude(playerbody.velocity, playerbody.velocity.magnitude - 3f * Time.deltaTime);
         }
         if (Input.GetKeyDown(KeyCode.F) && lastegg) {
             transform.position = lastegg.transform.position;
+            playerbody.velocity = lastegg.GetComponent<Rigidbody2D>().velocity;
+            
             GameObject.Destroy(lastegg);
         }
-    }
-
-    void FixedUpdate() {
-        // Time.deltaTime;
-        float horizontalInput = Input.GetAxis("Horizontal");
-        playerbody.velocity += Vector2.right * horizontalInput * speed;
     }
 }

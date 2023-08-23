c32eab100cafff4c41ed5686a8a76355b44eaddd
bokke1010 <bokke1010@gmail.com>
2023-08-23 13:08:16 +0200

    Add collision layers Add ground detection for jumps Add ground detection for throwing

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,23 +11,34 @@ public class PlayerMovement : MonoBehaviour
     public float jumpstrenght = 4.5f;
     public float launchspeed = 12f;
     public Transform egg;
+    public float heightTestPlayer;
+    public Collider2D groundCollider;
+    private bool hasEgg = true;
     private GameObject lastegg;
+    private int layerMaskGround;
     // Start is called before the first frame update
     void Start()
     {
         playerbody = gameObject.GetComponent<Rigidbody2D>();
+        layerMaskGround = LayerMask.GetMask("Ground");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump")) {
-            playerbody.AddForce(Vector2.up * jumpstrenght);
+
+        if (IsGrounded()) {
+            hasEgg = true;
+            if (Input.GetButtonDown("Jump")) {
+                playerbody.AddForce(Vector2.up * jumpstrenght);
+            }
         }
-        if (Input.GetMouseButtonDown(0)) {
+
+        if (Input.GetMouseButtonDown(0) && hasEgg) {
             if (lastegg) {
                 GameObject.Destroy(lastegg);
             }
+            hasEgg = false;
             
             Vector2 aimdir = (Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
             aimdir.Normalize();
@@ -50,14 +61,25 @@ public class PlayerMovement : MonoBehaviour
             ), playerbody.velocity.y
         );
 
-        if (Mathf.Abs(horizontalInput) < 0.2) {
+        if (Mathf.Abs(horizontalInput) < 0.2) { // Also affects vertical velocity
             playerbody.velocity = Vector2.ClampMagnitude(playerbody.velocity, playerbody.velocity.magnitude - 3f * Time.deltaTime);
         }
         if (Input.GetKeyDown(KeyCode.F) && lastegg) {
+            hasEgg = IsGrounded();
             transform.position = lastegg.transform.position;
             playerbody.velocity = lastegg.GetComponent<Rigidbody2D>().velocity;
             
             GameObject.Destroy(lastegg);
         }
     }
+
+    private bool IsGrounded() {
+        // Note that we only check for colliders on the Ground layer (we don't want to hit ourself). 
+        RaycastHit2D hit = Physics2D.Raycast(groundCollider.bounds.center, Vector2.down, heightTestPlayer, layerMaskGround);
+        bool isGrounded = hit.collider != null;
+        // It is soo easy to make misstakes so do a lot of Debug.DrawRay calls when working with colliders...
+        Debug.DrawRay(groundCollider.bounds.center, Vector2.down * heightTestPlayer, isGrounded ? Color.green : Color.red, 0.5f);
+        return isGrounded;
+    }
+
 }

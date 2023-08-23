aa6b4ebb59e78fc6decf6f0967e9cd438ef62016
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2023-08-23 21:31:14 +0200

    Main Menu/Pause Menu works

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    //[SerializeField] GameObject pauseMenu;
+
+    void Update (){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            if(GameIsPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+    
+    public void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f; //game time is paused.
+        GameIsPaused = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; //Game is unpaused.
+        GameIsPaused = false;
+    }
+
+    public void Quit(){
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(0);
     }
 }

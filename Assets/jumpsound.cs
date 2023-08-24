248ba20e20522a47deb6df22face6aa476a460a6
Constancekk <142896117+Constancekk@users.noreply.github.com>
2023-08-24 17:20:21 +0800

    Revert "sounds"
    This reverts commit ab1b478e28ac4ef83335254ab49e6c2c50111f63.

--- Assets/jumpsound.cs
+++ Assets/jumpsound.cs
@@ -4,32 +4,18 @@ using UnityEngine;
 
 public class jumpsound : MonoBehaviour
 {
-    public AudioSource audioSource1;
-    public AudioSource audioSource2;
-    public AudioSource audioSource3;
-
-
     // Start is called before the first frame update
     void Start()
     {
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         if(Input.GetKeyDown("space"))
 	{
-            audioSource1.Play();
-    }
-      if(Input.GetMouseButtonDown(0) == true)
-    {
-            audioSource2.Play();
-    }
-    if(Input.GetKeyDown(KeyCode.F))
-    {
-            audioSource3.Play();
-    }
+	GetComponent<AudioSource>().Play();
+    	}
     }
 }

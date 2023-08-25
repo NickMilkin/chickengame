4a1825614e58dbfd54eeed7b54d8f350f3c83530
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2023-08-25 15:59:44 +0200

    Fix instadeath

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -48,6 +48,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!selfRenderer.enabled) {
+            return;
+        }
 
         if (IsGrounded()) {
             hasEgg = true;

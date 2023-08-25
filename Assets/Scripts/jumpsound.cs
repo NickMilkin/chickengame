71a8211d4f78c09eb80c0a36784c25e95174adda
Constancekk <142896117+Constancekk@users.noreply.github.com>
2023-08-25 20:52:22 +0800

    sound fix

--- Assets/Scripts/jumpsound.cs
+++ Assets/Scripts/jumpsound.cs
@@ -22,6 +22,7 @@ public class jumpsound : MonoBehaviour
         
         if(Input.GetKeyDown("space") && movement.IsGrounded())
 	{
+            jumpSound.Stop();
             jumpSound.Play();
         }
         if(Input.GetMouseButtonDown(0))

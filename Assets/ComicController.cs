7e59fac13b5996034e6a8ac1da086460e9ab47bd
Constancekk <142896117+Constancekk@users.noreply.github.com>
2023-08-25 21:15:31 +0800

    comic sound

--- Assets/ComicController.cs
+++ Assets/ComicController.cs
@@ -8,6 +8,7 @@ public class ComicController : MonoBehaviour
     public GameObject[] images;
     public string nextScene;
     int activeIndex = 0;
+    public AudioSource Click;
 
     void Start() {
         images[0].SetActive(true);
@@ -16,6 +17,7 @@ public class ComicController : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
+            Click.Play();
             activeIndex += 1;
             if (activeIndex == images.Length) {
                 SceneManager.LoadScene(nextScene);

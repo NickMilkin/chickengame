81a61c78b22679ff18743a7cea2b192b96adba90
Mark Roberts <61202394+MarkRobbz@users.noreply.github.com>
2023-08-25 13:29:31 +0200

    Crossfade animation added to animation older

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,11 +6,20 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
 
+    public Animator transition;
+    public float transitionTime = 1;
+
     public string nextScene;
     // Start is called before the first frame update
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "Player") {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+
+        IEnumerator LoadLevel(int levelIndex){
+            transition.SetTrigger("Start"); //Scene change causes trigger of transistion.
+            yield return new WaitForSeconds(transitionTime);
+            if (other.gameObject.tag == "Player") {
             SceneManager.LoadScene(nextScene);
+            }
         }
     }
 }
